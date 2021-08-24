a3dc28ae949ade30afef1e5b0447e0fdec68ad70
Crami <cramitus@gmail.com>
2021-08-24 23:22:43 +0200

    Primeros Pasos el juego de la life
    Esta hecho una pequeña distribucion del hud para ver como quedara todo

--- Muy largo/Assets/Scripts/ManagersController.cs
+++ Muy largo/Assets/Scripts/ManagersController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ManagersController : MonoBehaviour
 {
+
+    public GameObject Difuminado;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ Muy largo/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+ 
+
+    public static void FadeIn (GameObject _blackScreen){
+
+        Animator animator = _blackScreen.GetComponent<Animator>();
+
+        animator.SetTrigger("FadeIn");
+    }
+
+    public static void FadeOut(GameObject _blackScreen)
+    {
+
+        Animator animator = _blackScreen.GetComponent<Animator>();
+
+        animator.SetTrigger("FadeOut");
+    }
+}
