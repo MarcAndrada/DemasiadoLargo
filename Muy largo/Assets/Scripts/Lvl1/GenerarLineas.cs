2e4f4ba80eca88a6af4da955faa347f42eed1fdc
Loproxi <79161178+Loproxi@users.noreply.github.com>
2021-08-26 21:25:27 +0200

    Lvl1 comienzo

---
+++ Muy largo/Assets/Scripts/Lvl1/GenerarLineas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerarLineas : MonoBehaviour
+{
+    public GameObject generarLineas;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 lineapos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            Instantiate(generarLineas, lineapos, transform.rotation);
+        }
+    }
+}
